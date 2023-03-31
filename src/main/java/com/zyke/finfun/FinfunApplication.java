f8164ccacdcdd4ea82aeaa1bbfb5aa2b0cb0beb2
zyke-99 <augustas.kersys@gmail.com>
2023-03-31 19:39:06 +0300

    Init commit

---
+++ src/main/java/com/zyke/finfun/FinfunApplication.java
@@ -0,0 +1,13 @@
+package com.zyke.finfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinfunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinfunApplication.class, args);
+	}
+
+}
